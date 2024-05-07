d6afbf397ac3217aaf131c6551a2a8687ae9aac4
MicheleSerafini <serafini.michele01@gmail.com>
2024-05-07 18:30:56 +0200

    FEAT > Carousel component + ProductCard component

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,44 +1,31 @@
 import { useRef } from 'react'
 import style from '../styles/modules/carousel.module.scss'
+import ProductCard from './ProductCard';
 
 export default function Carousel() {
 
   const carouselRef = useRef<HTMLDivElement | null>(null);
 
   const handleSliding = (direction: 'left'|'right'):void => {
+    
     if(carouselRef.current) {
       const visibleWidth = carouselRef.current.offsetWidth;
       const scrollAmount = direction === 'right' ? visibleWidth : -visibleWidth;
-      carouselRef.current.scroll({left: scrollAmount})
+      carouselRef.current.scrollLeft += scrollAmount;
     }
   }
 
   return (
     <div className={style.carousel}>
       <div className={style.carousel__showcase} ref={carouselRef}>
-        <div className={style.cardProva}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil cupiditate eius, iure commodi 
-        </div>
-        <div className={style.cardProva}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil cupiditate eius, iure commodi 
-        </div>
-        <div className={style.cardProva}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil cupiditate eius, iure commodi 
-        </div>
-        <div className={style.cardProva}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil cupiditate eius, iure commodi 
-        </div>
-        <div className={style.cardProva}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil cupiditate eius, iure commodi 
-        </div>
-        <div className={style.cardProva}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil cupiditate eius, iure commodi 
-        </div>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
       </div>
-      <div className={style.carousel__controls}>
-        <strong className={style.arrow} onClick={() => (handleSliding('left'))}>&#8592;</strong>
-        <strong className={style.arrow} onClick={() => (handleSliding('right'))}>&#8594;</strong>
-      </div>
+      <strong className={`${style.leftArrow} ${style.arrow}`} onClick={() => (handleSliding('left'))}>&#8592;</strong>
+      <strong className={`${style.rightArrow} ${style.arrow}`} onClick={() => (handleSliding('right'))}>&#8594;</strong>
     </div>
   )
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,7 +1,33 @@
+import style from '../styles/modules/productcard.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 export default function ProductCard() {
-  
+
   return (
-    <h1>card</h1>
+    <>
+    {/*TODO: INSERIRE I DATI DINAMICAMENTE*/}
+    <div className={style.productCard}>
+      <img className={style.productCard__image} src="/img/lamp_1.jpg" alt="Chair image"/>
+      <div className={style.productCard__info}>
+        <small className={style.product_type}>Lorem</small>
+        <a href="#">
+          <h4 className={style.product_name}>Lorem ipsum man</h4>
+        </a>
+        <div className={style.product_rating}>
+          {/* TODO: mettere le stelle */}
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+        </div>
+      </div>
+      <div className={style.productCard__add}>
+        <span className={style.product_price}>99 &euro;</span>
+        <button className={style.product_addBtn}>+</button>
+      </div>
+    </div>
+    </>
   )
 }
